f1cd8fc5228c50d56004c2a4385a2a8342fa18b7
Alexey Ardov <ardovalexey@gmail.com>
2021-07-22 17:03:36 +0300

    improve export

--- src/components/ExportButton.tsx
+++ src/components/ExportButton.tsx
@@ -1,15 +1,27 @@
 import React, { FC, useState } from 'react'
+import styled from 'styled-components'
 import { paletteToHex } from '../palette'
 import { Palette } from '../types'
 
 export const ExportButton: FC<{ palette: Palette }> = ({ palette }) => {
   const [visible, setVisible] = useState(false)
   return (
-    <>
+    <Wrapper>
       <button onClick={() => setVisible(v => !v)}>
         {visible ? 'Hide' : 'Export'}
       </button>
-      {visible && <pre>{JSON.stringify(paletteToHex(palette), null, 2)}</pre>}
-    </>
+      {visible && (
+        <textarea
+          onKeyDown={e => e.stopPropagation()}
+          value={JSON.stringify(paletteToHex(palette), null, 2)}
+        />
+      )}
+    </Wrapper>
   )
 }
+
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`
