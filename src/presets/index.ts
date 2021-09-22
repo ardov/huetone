9266bed4ed46fa1d04d39cd872ec86365b83baa7
Alexey Ardov <ardovalexey@gmail.com>
2021-09-22 20:53:46 +0300

    refactor presets

---
+++ src/presets/index.ts
@@ -0,0 +1,26 @@
+import { HexPalette } from '../types'
+import antDesign from './ant_design.json'
+import evergreen from './evergreen.json'
+import example from './example.json'
+import github from './github.json'
+import google from './google.json'
+import ibm from './ibm.json'
+import semrushOld from './semrush_old.json'
+import stripe from './stripe.json'
+import tailwindOld from './tailwind_old.json'
+import tailwindNew from './tailwind_new.json'
+import uswds from './uswds.json'
+
+export const PRESETS: HexPalette[] = [
+  example,
+  antDesign,
+  evergreen,
+  github,
+  google,
+  ibm,
+  semrushOld,
+  stripe,
+  tailwindOld,
+  tailwindNew,
+  uswds,
+]
