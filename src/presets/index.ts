0d9d380fc806831cee2a30e67005cc966155d919
Ivan Kozik <ivan@ludios.org>
2022-07-28 13:38:03 +0000

    add preset for Stripe Dashboard's colors
    These colors come from https://dashboard.stripe.com/

--- src/presets/index.ts
+++ src/presets/index.ts
@@ -7,6 +7,7 @@ import google from './google.json'
 import ibm from './ibm.json'
 import semrush from './semrush.json'
 import stripe from './stripe.json'
+import stripe_dashboard from './stripe_dashboard.json'
 import uswds from './uswds.json'
 import chakra_ui from './chakra_ui.json'
 import radix_ui_dark from './radix_ui_dark.json'
@@ -17,6 +18,7 @@ export const PRESETS: HexPalette[] = [
   example,
   semrush,
   stripe,
+  stripe_dashboard,
   chakra_ui,
   radix_ui_dark,
   radix_ui_light,
