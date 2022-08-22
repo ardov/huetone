58427a4f520f83780d9eb48566c119137d44918e
Alexey Ardov <ardovalexey@gmail.com>
2022-08-23 00:07:28 +0200

    fix config

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,7 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tsconfigPaths(), react()],
+  build: {
+    outDir: 'build',
+  },
 })
